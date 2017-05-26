f1ed54c3cc19f1c00d76ab1d2604039e0452c7a8
Max Chadwick <mpchadwick@gmail.com>
2017-05-26 19:12:03 -0400

    Don't tweet on startup

--- bots.rb
+++ bots.rb
@@ -18,8 +18,6 @@ class MyBot < Ebooks::Bot
   end
 
   def on_startup
-    model = Ebooks::Model.load("model/magento-all.model")
-    tweet(model.make_statement(140))
     scheduler.cron '0 6,18,22 * * * America/New_York' do
       tweet(model.make_statement(140))
       # pictweet("hi", "cuteselfie.jpg")
